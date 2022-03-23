e6acc600ae271fae4bc736a83a352614c7817701
Michael Huynh <mhuynh@twilio.com>
2022-03-22 17:30:14 -0700

    feat: finish common constants transform for java and js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,6 +1,6 @@
-const { readFile } = require('fs').promises;
+const { readFile, writeFile } = require('fs').promises;
 
-function denoteConstants(constantsSource) {
+function parseConstants(constantsSource) {
   const parse = (line) => {
     if (line.length === 0) {
       return { isWhitespace: true };
@@ -8,10 +8,10 @@ function denoteConstants(constantsSource) {
 
     const commentMatch = /^\s*\/\/ (?<comment>.*)$/.exec(line);
     if (commentMatch && commentMatch.groups.comment) {
-      return { isComment: true, line: commentMatch.groups.comment };
+      return { isComment: true, content: commentMatch.groups.comment };
     }
 
-    return { line };
+    return { isConstant: true, content: line };
   };
 
   const denotedConstants = constantsSource.split('\n').map(parse);
@@ -26,44 +26,142 @@ function denoteConstants(constantsSource) {
   return denotedConstants;
 }
 
-function templateConstants(templateSource) {
+function parseTemplate(templateSource) {
   const parse = (line) => {
     if (line.length === 0) {
       return { isWhitespace: true };
     }
 
-    const contentMatch = /^(?<prefix>.*){{CONTENT}}(?<postfix>.*)$/.exec(line);
-    if (contentMatch) {
-      return {
-        isContent: true,
-        prefix: contentMatch.groups.prefix,
-        postfix: contentMatch.groups.postfix,
-      };
+    const commentMatch = /{{COMMENT}}/.exec(line);
+    if (commentMatch) {
+      return { isComment: true, content: line };
     }
 
-    return { line };
+    const constantMatch = /{{CONSTANT}}/.exec(line);
+    if (constantMatch) {
+      return { isConstant: true, content: line };
+    }
+
+    return { content: line };
   };
 
   const denotedTemplate = templateSource.split('\n').map(parse);
+
+  const commentTemplates = denotedTemplate.reduce(
+    (acc, { isComment, content }, idx) =>
+      isComment ? [...acc, { content, idx }] : acc,
+    []
+  );
+  if (commentTemplates.length !== 1) {
+    throw new Error(
+      `Template file malformed. Incorrect number of lines \
+      (${commentTemplates.length}) describing "comment" template. \
+      Should be (1).`
+    );
+  }
+  const commentTemplateIdx = commentTemplates[0].idx;
+
+  const constantTemplates = denotedTemplate.reduce(
+    (acc, { isConstant, content }, idx) =>
+      isConstant ? [...acc, { content, idx }] : acc,
+    []
+  );
+  if (constantTemplates.length !== 1) {
+    throw new Error(
+      `Template file malformed. Incorrect number of lines \
+      (${constantTemplates.length}) describing "constant" template. \
+      Should be (1).`
+    );
+  }
+  const constantTemplateIdx = constantTemplates[0].idx;
+
+  if (commentTemplateIdx + 1 !== constantTemplateIdx) {
+    throw new Error(
+      'Template file malformed. \
+      Comment template and constant template must be sequential.'
+    );
+  }
+
+  denotedTemplate.splice(commentTemplateIdx, 2, {
+    isBody: true,
+  });
+
+  return {
+    denotedTemplate,
+    constantTemplate: constantTemplates[0].content,
+    commentTemplate: commentTemplates[0].content,
+  };
+}
+
+function merge(
+  denotedConstants,
+  denotedTemplate,
+  commentTemplate,
+  constantTemplate
+) {
+  const mapConstant = (constant) => {
+    if (constant.isWhitespace) {
+      return '';
+    }
+    if (constant.isComment) {
+      return String(commentTemplate).replace(/{{COMMENT}}/g, constant.content);
+    }
+    if (constant.isConstant) {
+      return String(constantTemplate).replace(
+        /{{CONSTANT}}/g,
+        constant.content
+      );
+    }
+  };
+
+  const mapTemplate = (templateLine) => {
+    if (templateLine.isWhitespace) {
+      return '';
+    }
+    if (templateLine.isBody) {
+      return denotedConstants.map(mapConstant);
+    }
+    return templateLine.content;
+  };
+
+  return denotedTemplate.flatMap(mapTemplate);
+}
+
+async function transform(constantsPath, templatePath, outputPath) {
+  const constantsSource = (await readFile(constantsPath)).toString('utf-8');
+
+  const templateSource = (await readFile(templatePath)).toString('utf-8');
+
+  const denotedConstants = parseConstants(constantsSource);
+
+  const { denotedTemplate, commentTemplate, constantTemplate } =
+    parseTemplate(templateSource);
+
+  const mergedConstants = merge(
+    denotedConstants,
+    denotedTemplate,
+    commentTemplate,
+    constantTemplate
+  );
+
+  await writeFile(outputPath, mergedConstants.join('\n'));
 }
 
 async function main() {
-  const constantsSource = (
-    await readFile('./constants/constants.src')
-  ).toString('utf-8');
+  const constantsPath = './constants/constants.src';
 
-  const templateSource = (
-    await readFile('./constants/constants.typescript.template')
-  ).toString('utf-8');
+  // Transform TS files
+  const tsTemplatePath = './constants/constants.typescript.template';
+  const tsOutputPath = './src/constants.ts';
 
-  const denotedConstants = denoteConstants(constantsSource);
+  await transform(constantsPath, tsTemplatePath, tsOutputPath);
 
+  // Transform Java files
+  const javaTemplatePath = './constants/constants.java.template';
+  const javaOutputPath =
+    './android/src/main/java/com/twiliovoicereactnative/CommonConstants.java';
 
-
-  console.log(denotedConstants);
-
-
-  // /{{CONSTANTS}}/.exec(consta)
+  await transform(constantsPath, javaTemplatePath, javaOutputPath);
 }
 
 main();
