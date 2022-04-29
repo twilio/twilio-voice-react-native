e520f0e28dd89d0ca3855bba5d9e4b3749394b42
Bobie Chen <bchen@twilio.com>
2022-04-29 11:37:39 -0700

    Update iOS getStats report structure

--- ios/Utilities/TwilioVoiceStatsReport.h
+++ ios/Utilities/TwilioVoiceStatsReport.h
@@ -9,112 +9,146 @@
 
 @interface TwilioVoiceStatsReport : NSObject
 
++ (NSArray *)jsonWithStatsReportsArray:(NSArray<TVOStatsReport *> *)statsReports;
+
 + (NSDictionary *)jsonWithStatsReport:(TVOStatsReport *)statsReport;
-+ (NSDictionary *)jsonWithLocalAudioTrackStats:(TVOLocalAudioTrackStats *)audioTrackStats;
-+ (NSDictionary *)jsonWithRemoteAudioTrackStats:(TVORemoteAudioTrackStats *)audioTrackStats;
-+ (NSDictionary *)jsonWithIceCandidateStats:(TVOIceCandidateStats *)iceCandidateStats;
-+ (NSDictionary *)jsonWithIceCandidatePairStats:(TVOIceCandidatePairStats *)iceCandidatePairStats;
++ (NSArray *)jsonWithLocalAudioTrackStats:(NSArray<TVOLocalAudioTrackStats *> *)audioTrackStatsArray;
++ (NSArray *)jsonWithRemoteAudioTrackStats:(NSArray<TVORemoteAudioTrackStats *> *)audioTrackStatsArray;
++ (NSArray *)jsonWithIceCandidateStats:(NSArray<TVOIceCandidateStats *> *)iceCandidateStatsArray;
++ (NSArray *)jsonWithIceCandidatePairStats:(NSArray<TVOIceCandidatePairStats *> *)iceCandidatePairStatsArray;
 
 @end
 
 
 @implementation TwilioVoiceStatsReport
 
++ (NSArray *)jsonWithStatsReportsArray:(NSArray<TVOStatsReport *> *)statsReports {
+    NSMutableArray *reportsArray = [NSMutableArray array];
+    for (TVOStatsReport *report in statsReports) {
+        [reportsArray addObject:[self jsonWithStatsReport:report]];
+    }
+    return reportsArray;
+}
+
 + (NSDictionary *)jsonWithStatsReport:(TVOStatsReport *)statsReport {
     return @{
         @"peerConnectionId": statsReport.peerConnectionId,
-        @"localAudioTrackStats": [self jsonWithLocalAudioTrackStats:statsReport.localAudioTrackStats[0]],
-        @"remoteAudioTrackStats": [self jsonWithRemoteAudioTrackStats:statsReport.remoteAudioTrackStats[0]],
-        @"iceCandidateStats": [self jsonWithIceCandidateStats:statsReport.iceCandidateStats[0]],
-        @"iceCandidatePairStats": [self jsonWithIceCandidatePairStats:statsReport.iceCandidatePairStats[0]]
+        @"localAudioTrackStats": [self jsonWithLocalAudioTrackStats:statsReport.localAudioTrackStats],
+        @"remoteAudioTrackStats": [self jsonWithRemoteAudioTrackStats:statsReport.remoteAudioTrackStats],
+        @"iceCandidateStats": [self jsonWithIceCandidateStats:statsReport.iceCandidateStats],
+        @"iceCandidatePairStats": [self jsonWithIceCandidatePairStats:statsReport.iceCandidatePairStats]
     };
 }
 
-+ (NSDictionary *)jsonWithLocalAudioTrackStats:(TVOLocalAudioTrackStats *)audioTrackStats {
-    return @{
-        // Base track stats
-        @"codec": audioTrackStats.codec,
-        @"packetsLost": @(audioTrackStats.packetsLost),
-        @"ssrc": audioTrackStats.ssrc,
-        @"timestamp": @(audioTrackStats.timestamp),
-        @"trackId": audioTrackStats.trackId,
-        
-        // Local track stats
-        @"bytesSent": @(audioTrackStats.bytesSent),
-        @"packetsSent": @(audioTrackStats.packetsSent),
-        @"roundTripTime": @(audioTrackStats.roundTripTime),
-        
-        // Local audio track stats
-        @"audioLevel": @(audioTrackStats.audioLevel),
-        @"jitter": @(audioTrackStats.jitter)
-    };
++ (NSArray *)jsonWithLocalAudioTrackStats:(NSArray<TVOLocalAudioTrackStats *> *)audioTrackStatsArray {
+    NSMutableArray *localAudioTrackStatsReport = [NSMutableArray array];
+    
+    for (TVOLocalAudioTrackStats *localAudioTrackStats in audioTrackStatsArray) {
+        [localAudioTrackStatsReport addObject:@{
+            // Base track stats
+            @"codec": localAudioTrackStats.codec,
+            @"packetsLost": @(localAudioTrackStats.packetsLost),
+            @"ssrc": localAudioTrackStats.ssrc,
+            @"timestamp": @(localAudioTrackStats.timestamp),
+            @"trackId": localAudioTrackStats.trackId,
+            
+            // Local track stats
+            @"bytesSent": @(localAudioTrackStats.bytesSent),
+            @"packetsSent": @(localAudioTrackStats.packetsSent),
+            @"roundTripTime": @(localAudioTrackStats.roundTripTime),
+            
+            // Local audio track stats
+            @"audioLevel": @(localAudioTrackStats.audioLevel),
+            @"jitter": @(localAudioTrackStats.jitter)
+        }];
+    }
+    
+    return localAudioTrackStatsReport;
 }
 
-+ (NSDictionary *)jsonWithRemoteAudioTrackStats:(TVORemoteAudioTrackStats *)audioTrackStats {
-    return @{
-        // Base track stats
-        @"codec": audioTrackStats.codec,
-        @"packetsLost": @(audioTrackStats.packetsLost),
-        @"ssrc": audioTrackStats.ssrc,
-        @"timestamp": @(audioTrackStats.timestamp),
-        @"trackId": audioTrackStats.trackId,
-        
-        // Remote track stats
-        @"bytesRecieved": @(audioTrackStats.bytesReceived),
-        @"packetsReceived": @(audioTrackStats.packetsReceived),
-        
-        // Remote audio track stats
-        @"audioLevel": @(audioTrackStats.audioLevel),
-        @"jitter": @(audioTrackStats.jitter),
-        @"mos": @(audioTrackStats.mos)
-    };
++ (NSArray *)jsonWithRemoteAudioTrackStats:(NSArray<TVORemoteAudioTrackStats *> *)audioTrackStatsArray {
+    NSMutableArray *remoteAudioTrackStatsReport = [NSMutableArray array];
+
+    for (TVORemoteAudioTrackStats *remoteAudioTrackStats in audioTrackStatsArray) {
+        [remoteAudioTrackStatsReport addObject:@{
+            // Base track stats
+            @"codec": remoteAudioTrackStats.codec,
+            @"packetsLost": @(remoteAudioTrackStats.packetsLost),
+            @"ssrc": remoteAudioTrackStats.ssrc,
+            @"timestamp": @(remoteAudioTrackStats.timestamp),
+            @"trackId": remoteAudioTrackStats.trackId,
+            
+            // Remote track stats
+            @"bytesRecieved": @(remoteAudioTrackStats.bytesReceived),
+            @"packetsReceived": @(remoteAudioTrackStats.packetsReceived),
+            
+            // Remote audio track stats
+            @"audioLevel": @(remoteAudioTrackStats.audioLevel),
+            @"jitter": @(remoteAudioTrackStats.jitter),
+            @"mos": @(remoteAudioTrackStats.mos)
+        }];
+    }
+    
+    return remoteAudioTrackStatsReport;
 }
 
-+ (NSDictionary *)jsonWithIceCandidateStats:(TVOIceCandidateStats *)iceCandidateStats {
-    return @{
-        @"candidateType": iceCandidateStats.candidateType,
-        @"deleted": @(iceCandidateStats.deleted),
-        @"ip": iceCandidateStats.ip,
-        @"isRemote": @(iceCandidateStats.isRemote),
-        @"port": @(iceCandidateStats.port),
-        @"priority": @(iceCandidateStats.priority),
-        @"protocol": iceCandidateStats.protocol,
-        @"transportId": iceCandidateStats.transportId,
-        @"url": iceCandidateStats.url
-    };
++ (NSArray *)jsonWithIceCandidateStats:(NSArray<TVOIceCandidateStats *> *)iceCandidateStatsArray {
+    NSMutableArray *iceCandidateStatsReport = [NSMutableArray array];
+    
+    for (TVOIceCandidateStats *iceCandidateStats in iceCandidateStatsArray) {
+        [iceCandidateStatsReport addObject:@{
+            @"candidateType": iceCandidateStats.candidateType,
+            @"deleted": @(iceCandidateStats.deleted),
+            @"ip": iceCandidateStats.ip,
+            @"isRemote": @(iceCandidateStats.isRemote),
+            @"port": @(iceCandidateStats.port),
+            @"priority": @(iceCandidateStats.priority),
+            @"protocol": iceCandidateStats.protocol,
+            @"transportId": iceCandidateStats.transportId,
+            @"url": iceCandidateStats.url
+        }];
+    }
+
+    return iceCandidateStatsReport;
 }
 
-+ (NSDictionary *)jsonWithIceCandidatePairStats:(TVOIceCandidatePairStats *)iceCandidatePairStats {
-    return @{
-        @"activeCandidatePair": @(iceCandidatePairStats.activeCandidatePair),
-        @"availableIncomingBitrate": @(iceCandidatePairStats.availableIncomingBitrate),
-        @"availableOutgoingBitrate": @(iceCandidatePairStats.availableOutgoingBitrate),
-        @"bytesReceived": @(iceCandidatePairStats.bytesReceived),
-        @"bytesSent": @(iceCandidatePairStats.bytesSent),
-        @"consentRequestsReceived": @(iceCandidatePairStats.consentRequestsReceived),
-        @"consentRequestsSent": @(iceCandidatePairStats.consentRequestsSent),
-        @"consentResponsesReceived": @(iceCandidatePairStats.consentResponsesReceived),
-        @"consentResponsesSent": @(iceCandidatePairStats.consentResponsesSent),
-        @"currentRoundTripTime": @(iceCandidatePairStats.currentRoundTripTime),
-        @"localCandidateId": iceCandidatePairStats.localCandidateId,
-        @"localCandidateIp": iceCandidatePairStats.localCandidateIp,
-        @"nominated": @(iceCandidatePairStats.nominated),
-        @"priority": @(iceCandidatePairStats.priority),
-        @"readable": @(iceCandidatePairStats.readable),
-        @"relayProtocol": iceCandidatePairStats.relayProtocol,
-        @"remoteCandidateId": iceCandidatePairStats.remoteCandidateId,
-        @"remoteCandidateIp": iceCandidatePairStats.remoteCandidateIp,
-        @"requestsReceieved": @(iceCandidatePairStats.requestsReceived),
-        @"requestsSent": @(iceCandidatePairStats.requestsSent),
-        @"responsesRecieved": @(iceCandidatePairStats.responsesReceived),
-        @"responsesSent": @(iceCandidatePairStats.responsesSent),
-        @"retransmissionsReceived": @(iceCandidatePairStats.retransmissionsReceived),
-        @"retransmissionsSent": @(iceCandidatePairStats.retransmissionsSent),
-        @"state": [self stringWithIceCandidatePairState:iceCandidatePairStats.state],
-        @"totalRoundTripTime": @(iceCandidatePairStats.totalRoundTripTime),
-        @"transportId": iceCandidatePairStats.transportId,
-        @"writeable": @(iceCandidatePairStats.writable)
-    };
++ (NSArray *)jsonWithIceCandidatePairStats:(NSArray<TVOIceCandidatePairStats *> *)iceCandidatePairStatsArray {
+    NSMutableArray *iceCandidatePairStatsReport = [NSMutableArray array];
+    
+    for (TVOIceCandidatePairStats *iceCandidatePairStats in iceCandidatePairStatsArray) {
+        [iceCandidatePairStatsReport addObject:@{
+            @"activeCandidatePair": @(iceCandidatePairStats.activeCandidatePair),
+            @"availableIncomingBitrate": @(iceCandidatePairStats.availableIncomingBitrate),
+            @"availableOutgoingBitrate": @(iceCandidatePairStats.availableOutgoingBitrate),
+            @"bytesReceived": @(iceCandidatePairStats.bytesReceived),
+            @"bytesSent": @(iceCandidatePairStats.bytesSent),
+            @"consentRequestsReceived": @(iceCandidatePairStats.consentRequestsReceived),
+            @"consentRequestsSent": @(iceCandidatePairStats.consentRequestsSent),
+            @"consentResponsesReceived": @(iceCandidatePairStats.consentResponsesReceived),
+            @"consentResponsesSent": @(iceCandidatePairStats.consentResponsesSent),
+            @"currentRoundTripTime": @(iceCandidatePairStats.currentRoundTripTime),
+            @"localCandidateId": iceCandidatePairStats.localCandidateId,
+            @"localCandidateIp": iceCandidatePairStats.localCandidateIp,
+            @"nominated": @(iceCandidatePairStats.nominated),
+            @"priority": @(iceCandidatePairStats.priority),
+            @"readable": @(iceCandidatePairStats.readable),
+            @"relayProtocol": iceCandidatePairStats.relayProtocol,
+            @"remoteCandidateId": iceCandidatePairStats.remoteCandidateId,
+            @"remoteCandidateIp": iceCandidatePairStats.remoteCandidateIp,
+            @"requestsReceieved": @(iceCandidatePairStats.requestsReceived),
+            @"requestsSent": @(iceCandidatePairStats.requestsSent),
+            @"responsesRecieved": @(iceCandidatePairStats.responsesReceived),
+            @"responsesSent": @(iceCandidatePairStats.responsesSent),
+            @"retransmissionsReceived": @(iceCandidatePairStats.retransmissionsReceived),
+            @"retransmissionsSent": @(iceCandidatePairStats.retransmissionsSent),
+            @"state": [self stringWithIceCandidatePairState:iceCandidatePairStats.state],
+            @"totalRoundTripTime": @(iceCandidatePairStats.totalRoundTripTime),
+            @"transportId": iceCandidatePairStats.transportId,
+            @"writeable": @(iceCandidatePairStats.writable)
+        }];
+    }
+
+    return iceCandidatePairStatsReport;
 }
 
 + (NSString *)stringWithIceCandidatePairState:(TVOIceCandidatePairState)state {
@@ -129,8 +163,6 @@
             return @"STATE_SUCCEEDED";
         case TVOIceCandidatePairStateWaiting:
             return @"STATE_WAITING";
-        case TVOIceCandidatePairStateCancelled:
-            return @"STATE_CANCELLED";
         case TVOIceCandidatePairStateUnknown:
             return @"STATE_UNKNOWN";
         default:
