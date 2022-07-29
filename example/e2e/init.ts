28a04d8a2628c8d10d5415d41f890c1c4fc66110
Bobie Chen <bchen@twilio.com>
2022-07-28 18:10:51 -0700

    Use Xcode 12.5 image

--- example/e2e/init.ts
+++ example/e2e/init.ts
@@ -5,7 +5,6 @@ import { init } from 'detox';
 const config = require('../package.json').detox;
 require('dotenv').config();
 jest.setTimeout(150000);
-jest.retryTimes(1);
 
 beforeAll(async () => {
   await device.uninstallApp();
