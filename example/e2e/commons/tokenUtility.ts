937c9cfa6f44713e1cd9a3c968eb262d6195a71a
Bobie Chen <bchen@twilio.com>
2022-08-09 16:32:42 -0700

    Token utility for e2e tests

---
+++ example/e2e/commons/tokenUtility.ts
@@ -0,0 +1,28 @@
+const twilio = require('../../node_modules/twilio');
+const config = require('../config.json');
+
+const outgoingApplicationSid = config.prod.twimlAppSid;
+const accountSid = config.prod.accountSid;
+const apiKeySid = config.prod.apiKeySid;
+const apiKeySecret = config.prod.apiKeySecret;
+const pushCredentialSid = config.prod.pushCredentialSid;
+
+export function generateAccessToken(identity: string) {
+  const accessToken = new twilio.jwt.AccessToken(
+    accountSid,
+    apiKeySid,
+    apiKeySecret, {
+      identity,
+    },
+  );
+
+  const voiceGrant = new twilio.jwt.AccessToken.VoiceGrant({
+    outgoingApplicationSid,
+    pushCredentialSid,
+  });
+
+  accessToken.addGrant(voiceGrant);
+  console.log(accessToken.toJwt());
+
+  return accessToken.toJwt();
+}
--- example/e2e/tests/voice/registration.voice.spec.ts
+++ example/e2e/tests/voice/registration.voice.spec.ts
@@ -1,6 +1,10 @@
+import {generateAccessToken} from '../../commons/tokenUtility.ts';
+
 describe('Registration APIs', () => {
     beforeAll(async () => {
         console.log("beforeAll");
+        let token = generateAccessToken('john');
+        console.log(token);
     });
     beforeEach(async () => {
         console.log("beforeEach");
