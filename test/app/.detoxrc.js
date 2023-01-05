3ea2c84cc9ae3bd900924a784959fa959fab3010
Bobie Chen <bchen@twilio.com>
2023-01-04 22:09:21 -0800

    fix: generate index.android.bundle

--- test/app/.detoxrc.js
+++ test/app/.detoxrc.js
@@ -33,10 +33,7 @@ module.exports = {
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android ; ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release ; cd ..',
-      reversePorts: [
-        8081
-      ]
+      build: 'cd android ; ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release ; cd ..'
     }
   },
   devices: {
