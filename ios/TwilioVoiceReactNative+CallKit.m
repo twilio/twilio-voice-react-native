22c9de0b664f68697c1a89c3e042f617dba25faa
Kumkum Bagchi <kbagchi@twilio.com>
2023-02-09 16:45:06 -0800

    feat: added a generic blurb about how displayName can be used when passed as a custom param

--- ios/TwilioVoiceReactNative+CallKit.m
+++ ios/TwilioVoiceReactNative+CallKit.m
@@ -37,7 +37,7 @@ NSString * const kCustomParametersKeyDisplayName = @"displayName";
 - (void)reportNewIncomingCall:(TVOCallInvite *)callInvite {
     self.callInviteMap[callInvite.uuid.UUIDString] = callInvite;
     
-    // Twilio Frontline specific logic
+    // If "displayName" is passed as a custom parameters in the TwiML application, it will be used as the caller name
     NSString *handleName = callInvite.from;
     NSDictionary *customParams = callInvite.customParameters;
     if (customParams[kCustomParametersKeyDisplayName]) {
