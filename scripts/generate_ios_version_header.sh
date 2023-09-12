b689e4dca4ddd11cba666516eec7010c2769d431
Bobie Chen <bchen@twilio.com>
2023-09-12 10:28:35 -0700

    fix: fix path in version generation script

--- scripts/generate_ios_version_header.sh
+++ scripts/generate_ios_version_header.sh
@@ -1,8 +1,5 @@
-BASE_DIR=`dirname $0`
-pushd "$BASE_DIR" >/dev/null
-
-REACT_NATIVE_SDK_VERSION="$(jq '.version' ../package.json)"
-VERSION_HEADER_FILE="../ios/TwilioVoiceReactNativeVersion.h"
+REACT_NATIVE_SDK_VERSION="$(jq '.version' package.json)"
+VERSION_HEADER_FILE="ios/TwilioVoiceReactNativeVersion.h"
 echo ${VERSION_HEADER_FILE}
 
 cat <<EOF >"${VERSION_HEADER_FILE}"
@@ -11,5 +8,3 @@ cat <<EOF >"${VERSION_HEADER_FILE}"
 #define TWILIO_VOICE_REACT_NATIVE_VERSION   @${REACT_NATIVE_SDK_VERSION}
 
 EOF
-
-popd  >/dev/null
