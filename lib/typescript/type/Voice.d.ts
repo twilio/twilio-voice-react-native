07624cc0cf206e1290df50134345198a888cdb2d
twilio-vblocks-ci <svc.vblocks-ci@twilio.com>
2023-12-13 06:35:58 +0000

    1.0.0-rc16

---
+++ lib/typescript/type/Voice.d.ts
@@ -0,0 +1,12 @@
+import type { Constants } from '../constants';
+import type { NativeAudioDevicesUpdatedEvent } from './AudioDevice';
+import type { NativeCallInviteEvent, NativeCallInviteAcceptedEvent, NativeCallInviteNotificationTappedEvent, NativeCallInviteRejectedEvent, NativeCancelledCallInviteEvent } from './CallInvite';
+import type { NativeErrorEvent } from './Error';
+export interface NativeRegisteredEvent {
+    type: Constants.VoiceEventRegistered;
+}
+export interface NativeUnregisteredEvent {
+    type: Constants.VoiceEventUnregistered;
+}
+export declare type NativeVoiceEvent = NativeAudioDevicesUpdatedEvent | NativeCallInviteEvent | NativeCallInviteAcceptedEvent | NativeCallInviteNotificationTappedEvent | NativeCallInviteRejectedEvent | NativeCancelledCallInviteEvent | NativeErrorEvent | NativeRegisteredEvent | NativeUnregisteredEvent;
+export declare type NativeVoiceEventType = Constants.VoiceEventAudioDevicesUpdated | Constants.VoiceEventCallInvite | Constants.VoiceEventCallInviteAccepted | Constants.VoiceEventCallInviteNotificationTapped | Constants.VoiceEventCallInviteRejected | Constants.VoiceEventCallInviteCancelled | Constants.VoiceEventError | Constants.VoiceEventRegistered | Constants.VoiceEventUnregistered;
