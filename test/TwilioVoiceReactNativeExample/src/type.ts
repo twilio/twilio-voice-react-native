323d5f33baa2f886f6fc2d37895d0292b35c30e7
Michael Huynh <12516091+mhuynh5757@users.noreply.github.com>
2024-01-19 13:03:38 -0800

    [VBLOCKS-2552] Upgrade test app (#278)
    * feat: bootstrap 0.72 RN version of test app
    
    * fix: update react dependencies
    
    * fix: add missing peer dependencies
    
    * fix: add unit tests to check command
    
    * fix: new import name

---
+++ test/TwilioVoiceReactNativeExample/src/type.ts
@@ -0,0 +1,34 @@
+import type { Call } from '@twilio/voice-react-native-sdk';
+
+export interface BoundCallMethod {
+  disconnect: () => void;
+  getStats: () => void;
+  hold: () => void;
+  mute: () => void;
+  postFeedback: (score: Call.Score, issue: Call.Issue) => () => void;
+  sendDigits: (digits: string) => void;
+}
+
+export interface BoundCallInfo {
+  customParameters: Record<any, any>;
+  from?: string;
+  to?: string;
+  state?: Call.State;
+  sid?: string;
+  isMuted?: boolean;
+  isOnHold?: boolean;
+}
+
+export interface BoundCallInvite {
+  accept: () => void;
+  callSid: string;
+  customParameters: Record<any, any>;
+  from: string;
+  to: string;
+  reject: () => void;
+}
+
+export interface EventLogItem {
+  id: string;
+  content: string;
+}
