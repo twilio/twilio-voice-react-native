dec9a65673e5ba879936dd7518c71d8c62d314bc
Michael Huynh <mhuynh@twilio.com>
2024-01-24 04:16:54 -0800

    feat: dummy detox test and circleci config

--- test/TwilioVoiceReactNativeExample/e2e/starter.test.js
+++ test/TwilioVoiceReactNativeExample/e2e/starter.test.js
@@ -9,17 +9,8 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it('should show the SDK version', async () => {
+    await expect(element(by.id('app_info'))).toBeVisible();
+    await expect(element(by.id('sdk_version'))).toBeVisible();
   });
 });
