ace46f32d1f9b9de45e49304fb29833783cc6185
Kevin <kchoy@twilio.com>
2024-02-14 14:05:20 -0800

    feat: emit callinfo onmessagereceived

--- android/src/main/java/com/twiliovoicereactnative/CallMessageListenerProxy.java
+++ android/src/main/java/com/twiliovoicereactnative/CallMessageListenerProxy.java
@@ -10,25 +10,32 @@ import static com.twiliovoicereactnative.CommonConstants.VoiceErrorKeyError;
 import static com.twiliovoicereactnative.CommonConstants.VoiceEventSid;
 import static com.twiliovoicereactnative.CommonConstants.VoiceEventType;
 import static com.twiliovoicereactnative.CommonConstants.JSEventKeyCallMessageInfo;
+import static com.twiliovoicereactnative.Constants.JS_EVENT_KEY_CALL_INFO;
 import static com.twiliovoicereactnative.JSEventEmitter.constructJSMap;
+import static com.twiliovoicereactnative.ReactNativeArgumentsSerializer.serializeCall;
 import static com.twiliovoicereactnative.ReactNativeArgumentsSerializer.serializeCallMessage;
 import static com.twiliovoicereactnative.ReactNativeArgumentsSerializer.serializeVoiceException;
+import static com.twiliovoicereactnative.VoiceApplicationProxy.getCallRecordDatabase;
 import static com.twiliovoicereactnative.VoiceApplicationProxy.getJSEventEmitter;
 
 import android.util.Pair;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
 import com.twilio.voice.Call;
 import com.twilio.voice.CallMessage;
 import com.twilio.voice.VoiceException;
 
+import com.twiliovoicereactnative.CallRecordDatabase.CallRecord;
+
+
 public class CallMessageListenerProxy implements Call.CallMessageListener {
   private static final SDKLog logger = new SDKLog(CallMessageListenerProxy.class);
 
   @Override
-  public void onMessageFailure(String voiceEventSID, VoiceException voiceException) {
+  public void onMessageFailure(String callSid, String voiceEventSID, VoiceException voiceException) {
     logger.debug("onMessageFailure");
 
     // notify JS layer
@@ -42,7 +49,7 @@ public class CallMessageListenerProxy implements Call.CallMessageListener {
   }
 
   @Override
-  public void onMessageSent(String voiceEventSID) {
+  public void onMessageSent(String callSid, String voiceEventSID) {
     logger.debug("onMessageSent");
 
     // notify JS layer
@@ -54,24 +61,38 @@ public class CallMessageListenerProxy implements Call.CallMessageListener {
   }
 
   @Override
-  public void onMessageReceived(CallMessage callMessage) {
+  public void onMessageReceived(String callSid, CallMessage callMessage) {
     logger.debug("onMessageReceived");
 
-    // notify JS layer ScopeCall
-    getJSEventEmitter().sendEvent(ScopeCall,
-      constructJSMap(
-        new Pair<>(VoiceEventType, CallEventMessageReceived),
-        new Pair<>(JSEventKeyCallMessageInfo, serializeCallMessage(callMessage))
-      )
-    );
+    for (CallRecord callRecord: getCallRecordDatabase().getCollection()) {
+      if (callRecord.getCallSid() == callSid &&
+        CallRecord.CallInviteState.ACTIVE == callRecord.getCallInviteState()) {
+        // notify JS layer ScopeCallInvite
+        getJSEventEmitter().sendEvent(ScopeCallInvite,
+          constructJSMap(
+            new Pair<>(VoiceEventType, CallEventMessageReceived),
+            new Pair<>(JSEventKeyCallMessageInfo, serializeCallMessage(callMessage))
+          )
+        );
+        return;
+      }
+    }
 
-    // notify JS layer ScopeCallInvite
-    getJSEventEmitter().sendEvent(ScopeCallInvite,
-      constructJSMap(
-        new Pair<>(VoiceEventType, CallEventMessageReceived),
-        new Pair<>(JSEventKeyCallMessageInfo, serializeCallMessage(callMessage))
-      )
-    );
+    for (CallRecord callRecord: getCallRecordDatabase().getCollection()) {
+      if (callRecord.getCallSid() == callSid) {
+        // notify JS layer ScopeCall
+        getJSEventEmitter().sendEvent(ScopeCall,
+          constructJSMap(
+            new Pair<>(VoiceEventType, CallEventMessageReceived),
+            new Pair<>(JS_EVENT_KEY_CALL_INFO, serializeCall(callRecord)),
+            new Pair<>(JSEventKeyCallMessageInfo, serializeCallMessage(callMessage))
+          )
+        );
+        return;
+      }
+    }
+
+    logger.debug(String.format("No match call or callInvite for %s", callSid));
   }
 
   private void sendJSEvent(@NonNull WritableMap event) {
