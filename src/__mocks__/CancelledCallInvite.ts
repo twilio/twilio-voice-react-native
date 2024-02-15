4ea73bc423bfc5fad3e17d4045ea51fa767d8e11
Michael Huynh <mhuynh@twilio.com>
2024-02-14 17:42:05 -0800

    feat: unit tests and test app

--- src/__mocks__/CancelledCallInvite.ts
+++
@@ -1,9 +0,0 @@
-import type { NativeCancelledCallInviteInfo } from '../type/CallInvite';
-
-export function createNativeCancelledCallInviteInfo(): NativeCancelledCallInviteInfo {
-  return {
-    callSid: 'mock-nativecancelledcallinviteinfo-callsid',
-    from: 'mock-nativecancelledcallinviteinfo-from',
-    to: 'mock-nativecancelledcallinviteinfo-to',
-  };
-}
