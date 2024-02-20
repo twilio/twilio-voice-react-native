9290b60e7c3218212646c2de632de87a4ff67346
Michael Huynh <12516091+mhuynh5757@users.noreply.github.com>
2024-02-20 12:27:58 -0800

    [VBLOCKS-2478] Call invite events tests (#315)
    * feat: unit tests and test app
    
    * feat: e2e tests for call invite events

--- src/__mocks__/CallInvite.ts
+++ src/__mocks__/CallInvite.ts
@@ -1,4 +1,9 @@
-import type { NativeCallInviteInfo } from '../type/CallInvite';
+import type {
+  NativeCallInviteInfo,
+  NativeCancelledCallInviteInfo,
+} from '../type/CallInvite';
+import { Constants } from '../constants';
+import { createNativeErrorInfo } from './Error';
 
 export function createNativeCallInviteInfo(): NativeCallInviteInfo {
   return {
@@ -13,4 +18,53 @@ export function createNativeCallInviteInfo(): NativeCallInviteInfo {
     from: 'mock-nativecallinviteinfo-from',
     to: 'mock-nativecallinviteinfo-to',
   };
+}
+
+export function createNativeCancelledCallInviteInfo(): NativeCancelledCallInviteInfo {
+  return {
+    callSid: 'mock-nativecallinviteinfo-callsid',
+    from: 'mock-nativecallinviteinfo-from',
+    to: 'mock-nativecallinviteinfo-to',
+  };
+}
+
+export function createMockNativeCallInviteEvents() {
+  return {
+    accepted: {
+      [Constants.CallInviteEventKeyType]:
+        Constants.CallInviteEventTypeValueAccepted,
+      [Constants.CallInviteEventKeyCallSid]:
+        'mock-nativecallinviteinfo-callsid',
+      callInvite: createNativeCallInviteInfo(),
+    },
+    notificationTapped: {
+      [Constants.CallInviteEventKeyType]:
+        Constants.CallInviteEventTypeValueNotificationTapped,
+      [Constants.CallInviteEventKeyCallSid]:
+        'mock-nativecallinviteinfo-callsid',
+      callInvite: createNativeCallInviteInfo(),
+    },
+    rejected: {
+      [Constants.CallInviteEventKeyType]:
+        Constants.CallInviteEventTypeValueRejected,
+      [Constants.CallInviteEventKeyCallSid]:
+        'mock-nativecallinviteinfo-callsid',
+      callInvite: createNativeCallInviteInfo(),
+    },
+    cancelled: {
+      [Constants.CallInviteEventKeyType]:
+        Constants.CallInviteEventTypeValueCancelled,
+      [Constants.CallInviteEventKeyCallSid]:
+        'mock-nativecallinviteinfo-callsid',
+      cancelledCallInvite: createNativeCancelledCallInviteInfo(),
+    },
+    cancelledWithError: {
+      [Constants.CallInviteEventKeyType]:
+        Constants.CallInviteEventTypeValueCancelled,
+      [Constants.CallInviteEventKeyCallSid]:
+        'mock-nativecallinviteinfo-callsid',
+      cancelledCallInvite: createNativeCancelledCallInviteInfo(),
+      error: createNativeErrorInfo(),
+    },
+  } as const;
 }
