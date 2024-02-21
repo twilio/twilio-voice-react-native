9ece80b6c4d5678990c715621fea66b92c8583b4
Kevin <kchoy@twilio.com>
2024-02-20 16:09:34 -0800

    fix: onmessagereceived java

--- android/src/main/java/com/twiliovoicereactnative/TwilioVoiceReactNativeModule.java
+++ android/src/main/java/com/twiliovoicereactnative/TwilioVoiceReactNativeModule.java
@@ -401,8 +401,7 @@ public class TwilioVoiceReactNativeModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void call_sendMessage(String uuid, String content, String contentType, String messageType, Promise promise) {
-    final CallRecord callRecord = validateCallRecord(reactContext, UUID.fromString(uuid), promise);
-    Objects.requireNonNull(callRecord);
+    final CallRecord callRecord = getCallRecordDatabase().get(new CallRecord(UUID.fromString(uuid)));
 
     final CallMessage.MessageType _messageType = validateMessageTypeFromString(messageType, promise);
     if (_messageType == null) {
@@ -415,7 +414,12 @@ public class TwilioVoiceReactNativeModule extends ReactContextBaseJavaModule {
 
     final CallMessage callMessage = new CallMessage.Builder(_messageType)
       .contentType(_contentType).content(content).build();
-    promise.resolve(callRecord.getVoiceCall().sendMessage(callMessage));
+
+    if (CallRecord.CallInviteState.ACTIVE == callRecord.getCallInviteState()) {
+      promise.resolve(callRecord.getCallInvite().sendMessage(callMessage));
+    } else {
+      promise.resolve(callRecord.getVoiceCall().sendMessage(callMessage));
+    }
   }
 
   // Register/UnRegister
