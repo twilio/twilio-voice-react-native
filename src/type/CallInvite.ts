4e9bbd4dee56956764c2235080c4cefdcf7eef0f
Michael Huynh <mhuynh@twilio.com>
2024-03-06 17:18:49 -0800

    fix: erroroneous duplicates from merge

--- src/type/CallInvite.ts
+++ src/type/CallInvite.ts
@@ -7,10 +7,6 @@ export interface BaseNativeCallInviteEvent {
   callSid: string;
 }
 
-export interface BaseNativeCallInviteEvent {
-  callSid: string;
-}
-
 export interface NativeCallInviteInfo {
   uuid: Uuid;
   callSid: string;
