6d6716e93de4cbece5ca4a70a0f6b84c75dcd0a8
Michael Huynh <mhuynh@twilio.com>
2024-11-26 11:48:39 -0800

    feat: unit tests

--- src/__tests__/Voice.test.ts
+++ src/__tests__/Voice.test.ts
@@ -794,6 +794,26 @@ describe('Voice class', () => {
         ).resolves.toBeUndefined();
       });
     });
+
+    describe('.setIncomingCallContactHandleTemplate', () => {
+      it('invokes the native module', async () => {
+        const template = 'Foo ${DisplayName}';
+        await new Voice().setIncomingCallContactHandleTemplate(template);
+        expect(
+          jest.mocked(
+            MockNativeModule.voice_setIncomingCallContactHandleTemplate
+          ).mock.calls
+        ).toEqual([[template]]);
+      });
+
+      it('returns a Promise<void>', async () => {
+        const setIncomingCallContactHandleTemplatePromise =
+          new Voice().setIncomingCallContactHandleTemplate('foobar');
+        await expect(
+          setIncomingCallContactHandleTemplatePromise
+        ).resolves.toBeUndefined();
+      });
+    });
   });
 
   describe('private methods', () => {
