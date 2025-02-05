1c149a1f23d6c0b8e25566064a7f6835c9831b3c
twilio-vblocks-ci <svc.vblocks-ci@twilio.com>
2025-02-05 22:38:39 +0000

    1.4.0-dev

--- lib/typescript/__tests__/error/generated.test.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * =============================================================================
- * NOTE(mhuynh): if tests fail in this file, ensure that errors have been
- * generated. Try `yarn run build:errors`.
- * =============================================================================
- */
-export {};
