1d26f77c5d2195ff6eb63ba58c1409a56e4e5b20
twilio-vblocks-ci <svc.vblocks-ci@twilio.com>
2025-06-18 22:00:59 +0000

    1.6.0

---
+++ lib/typescript/type/Call.d.ts
@@ -0,0 +1,59 @@
+import type { Constants } from '../constants';
+import type { CustomParameters, Uuid } from './common';
+import type { NativeErrorInfo } from './Error';
+import type { Call } from '../Call';
+import type { NativeCallMessageInfo } from './CallMessage';
+export interface NativeCallInfo {
+    uuid: Uuid;
+    customParameters?: CustomParameters;
+    from?: string;
+    [Constants.CallInfoInitialConnectedTimestamp]?: string;
+    isMuted?: boolean;
+    isOnHold?: boolean;
+    sid?: string;
+    state?: Call.State;
+    to?: string;
+}
+export interface NativeCallConnectedEvent {
+    type: Constants.CallEventConnected;
+    call: NativeCallInfo;
+}
+export interface NativeCallConnectFailureEvent {
+    type: Constants.CallEventConnectFailure;
+    call: NativeCallInfo;
+    error: NativeErrorInfo;
+}
+export interface NativeCallReconnectingEvent {
+    type: Constants.CallEventReconnecting;
+    call: NativeCallInfo;
+    error: NativeErrorInfo;
+}
+export interface NativeCallReconnectedEvent {
+    type: Constants.CallEventReconnected;
+    call: NativeCallInfo;
+}
+export interface NativeCallDisconnectedEvent {
+    type: Constants.CallEventDisconnected;
+    call: NativeCallInfo;
+    error?: NativeErrorInfo;
+}
+export interface NativeCallRingingEvent {
+    type: Constants.CallEventRinging;
+    call: NativeCallInfo;
+}
+export declare type NativeCallQualityWarnings = string[];
+export interface NativeCallQualityWarningsEvent {
+    type: Constants.CallEventQualityWarningsChanged;
+    call: NativeCallInfo;
+    [Constants.CallEventCurrentWarnings]: NativeCallQualityWarnings;
+    [Constants.CallEventPreviousWarnings]: NativeCallQualityWarnings;
+}
+export interface NativeCallMessageReceivedEvent {
+    type: Constants.CallEventMessageReceived;
+    call: NativeCallInfo;
+    [Constants.CallMessage]: NativeCallMessageInfo;
+}
+export declare type NativeCallEvent = NativeCallConnectedEvent | NativeCallConnectFailureEvent | NativeCallReconnectingEvent | NativeCallReconnectedEvent | NativeCallDisconnectedEvent | NativeCallRingingEvent | NativeCallQualityWarningsEvent | NativeCallMessageReceivedEvent;
+export declare type NativeCallEventType = Constants.CallEventConnectFailure | Constants.CallEventConnected | Constants.CallEventDisconnected | Constants.CallEventQualityWarningsChanged | Constants.CallEventReconnected | Constants.CallEventReconnecting | Constants.CallEventRinging | Constants.CallEventMessageReceived;
+export declare type NativeCallFeedbackIssue = Constants.CallFeedbackIssueAudioLatency | Constants.CallFeedbackIssueChoppyAudio | Constants.CallFeedbackIssueDroppedCall | Constants.CallFeedbackIssueEcho | Constants.CallFeedbackIssueNoisyCall | Constants.CallFeedbackIssueNotReported | Constants.CallFeedbackIssueOneWayAudio;
+export declare type NativeCallFeedbackScore = Constants.CallFeedbackScoreNotReported | Constants.CallFeedbackScoreOne | Constants.CallFeedbackScoreTwo | Constants.CallFeedbackScoreThree | Constants.CallFeedbackScoreFour | Constants.CallFeedbackScoreFive;
---
+++ lib/typescript/type/CallInvite.d.ts
@@ -0,0 +1,40 @@
+import type { Constants } from '../constants';
+import type { CustomParameters, Uuid } from './common';
+import type { NativeErrorInfo } from './Error';
+import type { NativeCallMessageInfo } from './CallMessage';
+export interface BaseNativeCallInviteEvent {
+    callSid: string;
+}
+export interface NativeCallInviteInfo {
+    uuid: Uuid;
+    callSid: string;
+    customParameters?: CustomParameters;
+    from: string;
+    to: string;
+}
+export interface NativeCallInviteAcceptedEvent extends BaseNativeCallInviteEvent {
+    type: Constants.CallInviteEventTypeValueAccepted;
+    callInvite: NativeCallInviteInfo;
+}
+export interface NativeCallInviteNotificationTappedEvent extends BaseNativeCallInviteEvent {
+    type: Constants.CallInviteEventTypeValueNotificationTapped;
+}
+export interface NativeCallInviteRejectedEvent extends BaseNativeCallInviteEvent {
+    type: Constants.CallInviteEventTypeValueRejected;
+    callInvite: NativeCallInviteInfo;
+}
+export interface NativeCallInviteCancelledEvent extends BaseNativeCallInviteEvent {
+    type: Constants.CallInviteEventTypeValueCancelled;
+    cancelledCallInvite: NativeCancelledCallInviteInfo;
+    error?: NativeErrorInfo;
+}
+export interface NativeCancelledCallInviteInfo {
+    callSid: string;
+    from: string;
+    to: string;
+}
+export interface NativeCallInviteMessageReceivedEvent extends BaseNativeCallInviteEvent {
+    type: Constants.CallEventMessageReceived;
+    [Constants.CallMessage]: NativeCallMessageInfo;
+}
+export declare type NativeCallInviteEvent = NativeCallInviteNotificationTappedEvent | NativeCallInviteAcceptedEvent | NativeCallInviteRejectedEvent | NativeCallInviteCancelledEvent | NativeCallInviteMessageReceivedEvent;
