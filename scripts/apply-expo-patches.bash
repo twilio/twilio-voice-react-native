fe46a65f51cbcb1ac9ec56b9d137cfa81b1a8eea
Michael Huynh <mhuynh@twilio.com>
2025-10-23 15:16:42 -0700

    feat: reorganize scripts

--- scripts/apply-expo-patches.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git apply expo-patches/ExpoActivityLifecycleListener.kt.diff &&
-git apply expo-patches/ExpoApplicationLifecycleListener.kt.diff &&
-git apply expo-patches/ExpoModule.kt.diff &&
-git apply expo-patches/ExpoPackage.kt.diff &&
-git apply expo-patches/build.gradle.diff &&
-git apply expo-patches/expo-module.config.json.diff &&
-git apply expo-patches/react-native.config.js.diff
