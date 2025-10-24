76d4fe5543d7c6b53a99c367428a68672c51e634
Michael Huynh <12516091+mhuynh5757@users.noreply.github.com>
2025-10-24 13:00:11 -0700

    [VBLOCKS-5519] feat: expo variant build scripts (#613)
    * feat: expo variant build scripts
    
    * feat: reorganize scripts

---
+++ scripts/expo/apply-patches.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git apply scripts/expo/patches/ExpoActivityLifecycleListener.kt.diff &&
+git apply scripts/expo/patches/ExpoApplicationLifecycleListener.kt.diff &&
+git apply scripts/expo/patches/ExpoModule.kt.diff &&
+git apply scripts/expo/patches/ExpoPackage.kt.diff &&
+git apply scripts/expo/patches/build.gradle.diff &&
+git apply scripts/expo/patches/expo-module.config.json.diff &&
+git apply scripts/expo/patches/react-native.config.js.diff
