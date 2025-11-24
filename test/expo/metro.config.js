002cbd612e1ad8be3661e9e16f08e4143502be07
Michael Huynh <mhuynh@twilio.com>
2025-11-24 12:39:38 -0800

    feat: adjust configs

---
+++ test/expo/metro.config.js
@@ -0,0 +1,33 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const escape = require('escape-string-regexp');
+const pak = require('../../package.json');
+
+const config = getDefaultConfig(__dirname);
+
+const root = path.resolve(__dirname, '../..');
+config.watchFolders = [root];
+
+const modules = Object.keys({ ...pak.peerDependencies });
+
+const blockList = exclusionList(
+  modules.map(
+    (m) =>
+      new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+  )
+);
+
+const extraNodeModules = modules.reduce((acc, name) => {
+  acc[name] = path.join(__dirname, 'node_modules', name);
+  return acc;
+}, {});
+
+config.resolver = {
+  ...config.resolver,
+  blockList,
+  extraNodeModules,
+};
+
+module.exports = config;
