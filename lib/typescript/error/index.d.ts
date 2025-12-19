a1846ba7a27309794fb8c4d5cb693e1b50bc2b96
twilio-vblocks-ci <svc.vblocks-ci@twilio.com>
2025-12-19 23:39:51 +0000

    2.0.0-rc2

---
+++ lib/typescript/error/index.d.ts
@@ -0,0 +1,6 @@
+export { InvalidArgumentError } from './InvalidArgumentError';
+export { InvalidStateError } from './InvalidStateError';
+export { UnexpectedNativeError } from './UnexpectedNativeError';
+export { UnsupportedPlatformError } from './UnsupportedPlatformError';
+export { TwilioError } from './TwilioError';
+export { AuthorizationErrors, ClientErrors, ForbiddenErrors, GeneralErrors, MalformedRequestErrors, MediaErrors, RegistrationErrors, ServerErrors, SignalingErrors, SIPServerErrors, TwiMLErrors, UserMediaErrors, } from './generated';
