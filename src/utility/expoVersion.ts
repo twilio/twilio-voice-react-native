f03de099e785ebfbc59ea5ac8a7cb56caebf5523
Michael Huynh <mhuynh@twilio.com>
2026-01-26 11:06:09 -0800

    feat: reduce check verbosity

--- src/utility/expoVersion.ts
+++ src/utility/expoVersion.ts
@@ -1,32 +1,15 @@
 export function getExpoVersion(): string | undefined {
   const expoManifest = global.expo?.modules?.ExponentConstants?.manifest;
 
-  // NOTE: iOS manifest value is a JS object.
-  if (
-    typeof expoManifest === 'object' &&
-    expoManifest !== null &&
-    typeof expoManifest.sdkVersion === 'string'
-  ) {
-    return expoManifest.sdkVersion;
+  if (!expoManifest) return undefined;
+
+  try {
+    const manifest =
+      typeof expoManifest === 'string'
+        ? JSON.parse(expoManifest)
+        : expoManifest;
+    return manifest?.sdkVersion;
+  } catch {
+    return undefined;
   }
-
-  // NOTE: Android manifest value is a JSON-encoded string.
-  if (typeof expoManifest === 'string') {
-    let expoManifestObj: any;
-    try {
-      expoManifestObj = JSON.parse(expoManifest);
-    } catch {
-      return;
-    }
-
-    if (
-      typeof expoManifestObj === 'object' &&
-      expoManifestObj !== null &&
-      typeof expoManifestObj.sdkVersion === 'string'
-    ) {
-      return expoManifestObj.sdkVersion;
-    }
-  }
-
-  return;
 }
