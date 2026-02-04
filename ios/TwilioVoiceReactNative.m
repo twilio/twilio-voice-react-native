77e022e623dfb91ab8b497ab7f81a44113f3ee54
Michael Huynh <mhuynh@twilio.com>
2026-02-03 17:12:33 -0800

    feat: add ios api for setting expo version

--- ios/TwilioVoiceReactNative.m
+++ ios/TwilioVoiceReactNative.m
@@ -701,6 +701,14 @@ RCT_EXPORT_METHOD(voice_setIncomingCallContactHandleTemplate:(NSString *)templat
     [self resolvePromise:resolver value:[NSNull null]];
 }
 
+RCT_EXPORT_METHOD(voice_setExpoVersion:(NSString *)expoVersion
+                  resolver:(RCTPromiseResolveBlock)resolver
+                  rejecter:(RCTPromiseRejectBlock)rejecter)
+{
+    setenv("com.twilio.voice.env.sdk.expo_version", [expoVersion UTF8String], 1);
+    [self resolvePromise:resolver value:[NSNull null]];
+}
+
 #pragma mark - Bindings (Call)
 
 - (TVOCall *)getCallFromMap:(NSString *)uuid resolver:(RCTPromiseResolveBlock)resolver {
