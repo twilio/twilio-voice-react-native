088d7665f8aafc733f7e8e3ca7ac093ab59dcfa8
twilio-vblocks-ci <svc.vblocks-ci@twilio.com>
2026-04-30 17:25:42 +0000

    2.0.0-dev

--- lib/typescript/__mocks__/AudioDevice.d.ts
+++
@@ -1,3 +0,0 @@
-import type { NativeAudioDeviceInfo, NativeAudioDevicesInfo } from '../type/AudioDevice';
-export declare function createNativeAudioDeviceInfo(): NativeAudioDeviceInfo;
-export declare function createNativeAudioDevicesInfo(): NativeAudioDevicesInfo;
