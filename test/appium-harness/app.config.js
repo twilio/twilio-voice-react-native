f270a85e1d3f47b375a18b1bb92b5762d1bd2ada
Michael Huynh <mhuynh@twilio.com>
2026-04-30 17:03:55 -0700

    fix: remove unnecessary expo boilerplate

--- test/appium-harness/app.config.js
+++ test/appium-harness/app.config.js
@@ -6,12 +6,9 @@ module.exports = {
     slug: 'twilio-voice-react-native-sdk-appium-harness',
     version: '1.0.0',
     orientation: 'portrait',
-    icon: './assets/images/icon.png',
     scheme: 'twiliovoicereactnativesdkappiumharness',
-    userInterfaceStyle: 'automatic',
     newArchEnabled: true,
     ios: {
-      icon: './assets/expo.icon',
       supportsTablet: true,
       bundleIdentifier: 'com.twilio.TwilioVoiceReactNativeExample',
       appleTeamId: secrets.appleTeamId,
@@ -24,28 +21,11 @@ module.exports = {
       },
     },
     android: {
-      adaptiveIcon: {
-        backgroundColor: '#E6F4FE',
-        foregroundImage: './assets/images/android-icon-foreground.png',
-        backgroundImage: './assets/images/android-icon-background.png',
-        monochromeImage: './assets/images/android-icon-monochrome.png'
-      },
-      predictiveBackGestureEnabled: false,
       package: 'com.example.twiliovoicereactnative',
       googleServicesFile: './google-services.json',
     },
     plugins: [
       'expo-router',
-      [
-        'expo-splash-screen',
-        {
-          backgroundColor: '#208AEF',
-          android: {
-            image: './assets/images/splash-icon.png',
-            imageWidth: 76
-          }
-        }
-      ]
     ],
     experiments: {
       typedRoutes: true,
